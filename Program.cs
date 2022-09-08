cba06f8bb61dac5fbd922892597a565786ff91c2
rodocarm <rcarmen@my.wctc.edu>
2022-09-08 10:15:01 -0500

    final commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using NLog.Web;
 
 namespace SleepData
 {
@@ -7,6 +8,13 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
+            String file = "data.txt";
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("Program Started");
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
@@ -17,11 +25,22 @@ namespace SleepData
             if (resp == "1")
             {
                 // create data file
-
-                 // ask a question
+                bool isVaildNumberOfWeeks = false;
+                int weeks = 0;
+                while(!isVaildNumberOfWeeks)
+                {
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
-                int weeks = int.Parse(Console.ReadLine());
+                if (int.TryParse(Console.ReadLine(), out weeks)) {
+                    isVaildNumberOfWeeks = true;
+                }
+                else
+                {
+                    logger.Error("Invaild input");
+                }
+                }
+                 // ask a question
+
 
                  // determine start and end date
                 DateTime today = DateTime.Now;
@@ -55,9 +74,23 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
-
+                if(File.Exists(file))
+                {
+                    StreamReader sr = new StreamReader(file);
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] arr = line.Split(',','|');
+                            DateTime d = DateTime.Parse(arr[0]);
+                            Console.WriteLine("Week of {0:MMM}, {0:dd}, {0:yyyy}", d);
+                            Console.WriteLine("Mo Tu We Th Fr Sa Su");
+                            Console.WriteLine("-- -- -- -- -- -- --");
+                            Console.WriteLine("{0}  {1}  {2}  {3}  {4}  {5}  {6}",arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7]);
+                        }
+                        sr.Close();
+                }
             }
+            logger.Info("Program ended");
         }
     }
 }
